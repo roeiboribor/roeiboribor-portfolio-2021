c3032462a2cc00d917a2415b4ec5d1210873e297
roeiboribor <roeiboribor@gmail.com>
2021-09-30 16:36:02 +0800

    Add Test page

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function home()
+    {
+        return view('home');
+    }
+
+    public function test()
+    {
+        return view('pages.test');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,8 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [PageController::class, 'home'])->name('home');
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/test', [PageController::class, 'test'])->name('test');
 });
 
 Route::get('/dashboard', function () {
