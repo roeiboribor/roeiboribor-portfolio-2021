00f4cff4ea20b19fcbaea4f9d7919e60d1f6e9eb
roeiboribor <roeiboribor@gmail.com>
2021-09-30 12:58:05 +0800

    php migrate add columns to users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,11 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->boolean('is_active');
+            $table->string('role');
+            $table->string('employee_id')->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
