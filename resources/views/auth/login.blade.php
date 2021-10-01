9eb21c67b0859a83a6e3a00bf46421ac7da619d4
roeiboribor <roeiboribor@gmail.com>
2021-10-01 09:58:43 +0800

    Change the route of login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -36,11 +36,9 @@
             </div>
 
             <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
+                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('register') }}">
+                    {{ __('No account yet?') }}
                 </a>
-                @endif
 
                 <x-button class="ml-3">
                     {{ __('Log in') }}
