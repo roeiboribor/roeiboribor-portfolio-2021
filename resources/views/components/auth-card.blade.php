8cc71f03cd9c3eeb76d68158c6360d66ca766867
roeiboribor <roeiboribor@gmail.com>
2021-10-23 17:12:17 +0800

    Reformat Components class

--- resources/views/components/auth-card.blade.php
+++ resources/views/components/auth-card.blade.php
@@ -1,5 +1,5 @@
 <div
-    class="min-h-screen flex justify-center items-center text-smalt-900 dark:text-gray-100 transition-colors duration-300 bg-gray-100 dark:bg-smalt-900">
+    class="min-h-screen flex justify-center items-center text-smalt-900 dark:text-gray-100 transition-colors duration-300 bg-gray-300 dark:bg-smalt-900">
     <div class="relative w-full mx-4 sm:max-w-md">
         <div class="px-6 py-4 bg-white dark:bg-smalt-900 shadow-md sm:rounded-lg relative z-10">
             {{ $slot }}
