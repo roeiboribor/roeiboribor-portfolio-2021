96bd2657ca0251585f008dd17e69e2f3a928b2f9
roeiboribor <roeiboribor@gmail.com>
2021-10-23 17:12:27 +0800

    Add Middleware for role

--- routes/web.php
+++ routes/web.php
@@ -10,9 +10,7 @@ use Illuminate\Support\Facades\Route;
 | Web Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| Add Middleware role to limit the user
 |
 */
 
