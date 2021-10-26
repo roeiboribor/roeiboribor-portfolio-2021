eda1c821481da4d445fe9cef99b30a8931afb35a
roeiboribor <roeiboribor@gmail.com>
2021-10-26 13:24:35 +0800

    Change text color

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -9,31 +9,31 @@
             <div>
                 <x-label for="first_name" :value="__('First Name')" />
 
-                <x-input id="first_name" class="block mt-1 w-full text-smalt-600" type="text" name="first_name"
+                <x-input id="first_name" class="block mt-1 w-full text-smalt-700" type="text" name="first_name"
                     :value="old('first_name')" placeholder="Enter First Name" required autofocus />
             </div>
             <div class="mt-4">
                 <x-label for="last_name" :value="__('Last Name')" />
 
-                <x-input id="last_name" class="block mt-1 w-full text-smalt-600" type="text" name="last_name"
+                <x-input id="last_name" class="block mt-1 w-full text-smalt-700" type="text" name="last_name"
                     :value="old('last_name')" placeholder="Enter Last Name" required autofocus />
             </div>
             <div class="mt-4">
                 <x-label for="email" :value="__('Email')" />
 
-                <x-input id="email" class="block mt-1 w-full text-smalt-600" type="email" name="email"
+                <x-input id="email" class="block mt-1 w-full text-smalt-700" type="email" name="email"
                     :value="old('email')" placeholder="Enter Email ex. first_last@test.com" required />
             </div>
             <div class="mt-4">
                 <x-label for="password" :value="__('Password')" />
 
-                <x-input id="password" class="block mt-1 w-full text-smalt-600" type="password" name="password" required
+                <x-input id="password" class="block mt-1 w-full text-smalt-700" type="password" name="password" required
                     autocomplete="new-password" />
             </div>
             <div class="mt-4">
                 <x-label for="password_confirmation" :value="__('Confirm Password')" />
 
-                <x-input id="password_confirmation" class="block mt-1 w-full text-smalt-600" type="password"
+                <x-input id="password_confirmation" class="block mt-1 w-full text-smalt-700" type="password"
                     name="password_confirmation" required />
             </div>
             <div class="flex items-center justify-end mt-4">
