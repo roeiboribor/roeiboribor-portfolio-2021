ecd021d6626fd745dbc9c19dc487c1353cca4829
roeiboribor <roeiboribor@gmail.com>
2021-10-29 16:00:19 +0800

    Edit blocked quotes

--- resources/views/components/portfolio/about-section.blade.php
+++ resources/views/components/portfolio/about-section.blade.php
@@ -5,10 +5,14 @@
     <div class="relative z-10 card p-4 md:p-8 rounded-xl flex shadow-lg">
         <i class='bx bxs-quote-left text-2xl text-smalt-200'></i>
         <blockquote class="p-2 md:p-4 tracking-wide text-center">
-            A creative and detailed individual with ability to develop effective and
-            efficient solutions with zero tolerance for
-            errors. Looking to join a progressive tech company where my technical skills
-            will be harnessed & utilize.
+            Hi! I'm a self-taught PHP Developer with a 1+ year hands on experience of building web application using
+            PHP. My
+            experience as a self-taught involves building scalable server side web apps using framework like Laravel and
+            uses
+            version control like GitHub to upload my local projects. I have an advance knowledge
+            when creating Login Forms, Inventory System, API & Cloning web apps. I also have basic hands on experience
+            using NPM,
+            Artisan commands, FileZilla, Putty & CLI commands.
         </blockquote>
         <i class='bx bxs-quote-right text-2xl text-smalt-200'></i>
     </div>
