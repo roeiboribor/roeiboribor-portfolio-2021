d11f56485be5008326839abce9bcca20823f7d2a
roeiboribor <roeiboribor@gmail.com>
2021-11-14 18:08:20 +0800

    Rename the BlogController to PostController

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class BlogController extends Controller
+class PostController extends Controller
 {
     /**
      * Display a listing of the resource.
